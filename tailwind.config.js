de1450a0d941c6fe2da1ed903e974c70335eea8f
bambang3 <bambang@timkado.id>
2022-04-07 12:33:40 +0700

    ganti warna primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,18 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
-        secondary: '#64748b',
-        dark: '#0f172a'          
+        primary: "#1e40af",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
       screens: {
-        '2xl' : '1320px'
-      } 
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
+};
